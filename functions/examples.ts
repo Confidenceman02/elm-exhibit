9654f246e74d93f2da941d6dd1d118fb3171cf48
Jaime Terreu <jaime@terreu.com>
2020-10-06 00:43:08 +1030

    Modify examples endpoint

--- functions/examples.ts
+++ functions/examples.ts
@@ -1,9 +1,10 @@
 import examples from "../data/examples.json"
+import fs from "fs"
 
 export async function handler() {
   return {
     statusCode: 200,
-    body: JSON.stringify(examples),
+    body: JSON.stringify({examples: examples}),
     headers: {
       "Content-Type": "application/json"
     }
