18e2152bcf06acb8906e1745cc91fc31bc11648a
Jaime Terreu <jaime@terreu.com>
2020-10-05 15:51:18 +1030

    Add Api and Endpoint modules

---
+++ src/Api/Api.elm
@@ -0,0 +1,10 @@
+module Api.Api exposing (..)
+
+import Api.Endpoint exposing (Endpoint, unwrap)
+import Http
+import Json.Decode exposing (Decoder)
+
+
+get : Endpoint -> (Result Http.Error a -> msg) -> Decoder a -> Cmd msg
+get url toMsg decoder =
+    Http.get { url = unwrap url, expect = Http.expectJson toMsg decoder }
--- src/Api/Endpoint.elm
+++ src/Api/Endpoint.elm
@@ -1,1 +1,23 @@
-module Api/Endpoints exposing (..)
+module Api.Endpoint exposing (Endpoint, lambdaEndpoint, lambdaUrl, unwrap)
+
+import Url.Builder exposing (QueryParameter)
+
+
+type Endpoint
+    = Endpoint String
+
+
+unwrap : Endpoint -> String
+unwrap (Endpoint ep) =
+    ep
+
+
+lambdaEndpoint : String
+lambdaEndpoint =
+    "/.netlify/functions"
+
+
+lambdaUrl : List String -> List QueryParameter -> Endpoint
+lambdaUrl path params =
+    Url.Builder.absolute (lambdaEndpoint :: path) params
+        |> Endpoint
