43e9c2814fda0f064c03076ed2db630e0bbdf54f
Jaime Terreu <jaime@terreu.com>
2020-10-10 13:17:43 +1030

    Added tokens for font weight and font size

--- src/Styles/Typography.elm
+++ src/Styles/Typography.elm
@@ -1,8 +1,18 @@
-module Styles.Typography exposing (exTypographyButtonSecondaryFontSize)
+module Styles.Typography exposing (exTypographyButtonSecondaryFontSize, exTypographyHeadingFontSize, exTypographyHeadingFontWeight)
 
-import Css exposing (Rem)
+import Css exposing (IntOrAuto, Rem)
 
 
 exTypographyButtonSecondaryFontSize : Rem
 exTypographyButtonSecondaryFontSize =
     Css.rem 1
+
+
+exTypographyHeadingFontSize : Rem
+exTypographyHeadingFontSize =
+    Css.rem 1.25
+
+
+exTypographyHeadingFontWeight : Int
+exTypographyHeadingFontWeight =
+    600
