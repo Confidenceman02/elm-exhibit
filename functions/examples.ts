1f95246c3f3ae1e3a1d0ed03db1a40a59e8c2d0c
Jaime Terreu <jaime@terreu.com>
2020-10-10 22:48:31 +1030

    Handle package and author from params

--- functions/examples.ts
+++ functions/examples.ts
@@ -1,12 +1,30 @@
-import examples from "../data/examples.json"
-import fs from "fs"
+import examples from "../data/examples.json";
+import {APIGatewayEvent, Context} from 'aws-lambda';
+import { StatusCodes } from "http-status-codes";
 
-export async function handler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({examples: examples}),
-    headers: {
-      "Content-Type": "application/json"
+export async function handler(event: APIGatewayEvent, context: Context) {
+  const params = event.queryStringParameters;
+
+  if (!params) {
+    return errorResponse(StatusCodes.BAD_REQUEST, "Missing author and package parameters" )
+  }
+
+  if (params.author && params.package) {
+    return {
+      statusCode: StatusCodes.OK,
+      body: JSON.stringify({examples: examples}),
+      headers: {
+        "Content-Type": "application/json"
+      }
     }
+  } else {
+    return errorResponse(StatusCodes.BAD_REQUEST, "Missing author and package parameters")
+  }
+}
+
+function errorResponse(statusCode: StatusCodes, errMsg: string) {
+  return {
+    statusCode: statusCode,
+    body: { error: errMsg }
   }
 }
