c98ca51146a36e45cc429ab66da9f274baf8d4c5
Jaime Terreu <jaime@terreu.com>
2020-10-25 01:42:09 +1030

    Return fake session when theres a cookie

--- functions/session-refresh.ts
+++ functions/session-refresh.ts
@@ -1,7 +1,7 @@
-import {StatusCodes} from "http-status-codes";
-import {APIGatewayEvent, Context} from "aws-lambda";
-import {ResponseBody} from "./types";
-import {errorResponse, jsonHeaders} from "./common";
+import { StatusCodes } from "http-status-codes";
+import { APIGatewayEvent, Context } from "aws-lambda";
+import { ResponseBody } from "./types";
+import { errorResponse, jsonHeaders } from "./common";
 
 export async function handler(event: APIGatewayEvent, context: Context): Promise<ResponseBody> {
   const { cookie } = event.headers
@@ -9,7 +9,7 @@ export async function handler(event: APIGatewayEvent, context: Context): Promise
   if (cookie) {
     return {
       statusCode: StatusCodes.OK,
-      body: JSON.stringify({name: "Jaime", id: "123"}),
+      body: JSON.stringify({name: "Jaime", id: "123", tag: "SessionRefreshed"}),
       headers: { ...jsonHeaders }
     }
   } else {
