9769898a45bea507bccc60e4c1df66654d3e675d
Jaime Terreu <jaime@terreu.com>
2020-10-27 21:37:19 +1030

    Persisting temporary session meta
    This ensures we can retrieve session specific metadata like where was
    the user in the app when they tried to log in. We can redirect them to
    that same page after auth.

--- functions/session-grant.ts
+++ functions/session-grant.ts
@@ -3,20 +3,27 @@ import { RedirectBody, ResponseBody } from "./types";
 import { StatusCodes } from "http-status-codes";
 import { URL } from "url";
 import { errorResponse, jsonHeaders } from "./common";
+import { v4 as uuidv4 } from "uuid"
+import {initTempSession} from "./redis/actions";
 
 const gitHubClientId: string | undefined = process.env.GITHUB_CLIENT_ID
 const githubLoginEndpoint: URL = new URL("https://github.com/login/oauth/authorize")
 
 export async function handler(event: APIGatewayEvent, context: Context): Promise<ResponseBody> {
+  const { referer } = event.headers
   if (gitHubClientId) {
-    console.log(event.headers)
+    const sessionId = uuidv4()
     githubLoginEndpoint.searchParams.append("client_id", gitHubClientId)
+    // save temporary session meta
+    const tempSession = await initTempSession({ sessionId, referer } )
 
-    return {
-      statusCode: StatusCodes.OK,
-      body: JSON.stringify({ tag: "Redirecting", location: githubLoginEndpoint.href }),
-      headers: {
-        ...jsonHeaders
+    if (tempSession) {
+      return {
+        statusCode: StatusCodes.OK,
+        body: JSON.stringify({ tag: "Redirecting", location: githubLoginEndpoint.href }),
+        headers: {
+          ...jsonHeaders
+        }
       }
     }
   }
