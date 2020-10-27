0bba6de63d8b64c9c292400e16d599f3a57c90d5
Jaime Terreu <jaime@terreu.com>
2020-10-27 21:40:51 +1030

    Add redis helper to persist temp session

---
+++ functions/redis/actions.ts
@@ -0,0 +1,18 @@
+import client from "./client"
+import {generateDBKey} from "./common";
+import {DBTag} from "./types";
+
+type TempSessionMeta =
+  {
+    sessionId: string,
+    referer: string
+  }
+// This will store the referer so that when the user approves the github app we can
+// redirect them back to where they tried to login from. i.e. example page
+export async function initTempSession(meta: TempSessionMeta): Promise<boolean> {
+  const dbKey = generateDBKey(DBTag.TempSession, meta.sessionId)
+  const setSession = await client.HSETAsync(dbKey, "referer", meta.referer )
+  client.EXPIRE(dbKey, 10)
+
+  return !!setSession
+}
