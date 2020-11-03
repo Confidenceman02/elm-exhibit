f22dcf23d8b1ea35b84cfdaa43e0a7e1b83e9788
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-03 23:28:23 +1030

    Remove comments

--- index.ts
+++ index.ts
@@ -1,31 +1,13 @@
-import { Elm } from './src/Main.elm'
+import {Elm} from './src/Main'
 
-var app = Elm.Main.init({node: document.querySelector('main'), flags: {}});
+const app: Elm.Main.App = Elm.Main.init({node: document.querySelector('main'), flags: null});
 
-app.ports.decodeRefererFromStateParam.subscribe((base64String) => {
+app.ports.decodeRefererFromStateParam.subscribe((base64String: string) => {
     const decodedString = window.atob(base64String)
     try {
         const parsedString = JSON.parse(decodedString)
         console.log(decodedString)
     } catch (e) {
-       throw e
+        console.log(e)
     }
 })
-// app.ports.storeCache.subscribe(function(val) {
-//
-//   if (val === null) {
-//     localStorage.removeItem(storageKey);
-//   } else {
-//     localStorage.setItem(storageKey, JSON.stringify(val));
-//   }
-//
-//   // Report that the new session was stored successfully.
-//   setTimeout(function() { app.ports.onStoreChange.send(val); }, 0);
-// });
-//
-// // Whenever localStorage changes in another tab, report it if necessary.
-// window.addEventListener("storage", function(event) {
-//   if (event.storageArea === localStorage && event.key === storageKey) {
-//     app.ports.onStoreChange.send(event.newValue);
-//   }
-// }, false);
