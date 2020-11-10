b4f2e16c24cbb429ba0512151d16a4f2f1c1d725
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-10 23:52:11 +1030

    Remove Result function
    This has been moved to lib

--- functions/common.ts
+++ functions/common.ts
@@ -1,12 +1,3 @@
-import {ResultResolver, Status} from "./types";
-
 export function removeWhiteSpace(value: string): string {
   return value.replace(/\s/g, '')
-}
-
-export function Result<T>(): ResultResolver<T> {
-  return {
-    Err: { Status: Status.Err },
-    Ok: (arg: T) => ({ Status: Status.Ok, data: arg })
-  }
 }
