43959a43a57a4dd94e645fdcd45394209600e74b
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-10 23:52:48 +1030

    Create githubuser enpoint helper

--- functions/endpoint.ts
+++ functions/endpoint.ts
@@ -1,12 +1,13 @@
-import {ResultType} from "./types";
-import {Result} from "./common";
+import {Result, ResultType} from "../lib/result";
+import { URL } from "url";
 
 const GITHUB_OAUTH_LOGIN_ENDPOINT =  "https://github.com/login/oauth/access_token"
+const GITHUB_USER_ENDPOINT =  "https://api.github.com/user"
 const GITHUB_CLIENT_ID: string | undefined = process.env.GITHUB_CLIENT_ID
 const GITHUB_CLIENT_SECRET: string | undefined = process.env.GITHUB_CLIENT_SECRET
 
 export function githubLoginEndpoint(code: string): ResultType<URL> {
-  const endpointUrl = new URL(GITHUB_OAUTH_LOGIN_ENDPOINT)
+  const endpointUrl: URL = new URL(GITHUB_OAUTH_LOGIN_ENDPOINT)
   endpointUrl.searchParams.append("code", code)
   if (GITHUB_CLIENT_ID && GITHUB_CLIENT_SECRET) {
     endpointUrl.searchParams.append("client_id", GITHUB_CLIENT_ID)
@@ -15,4 +16,8 @@ export function githubLoginEndpoint(code: string): ResultType<URL> {
   } else {
     return Result().Err
   }
+}
+
+export function githubUserEndpoint(): URL {
+  return new URL(GITHUB_USER_ENDPOINT)
 }
