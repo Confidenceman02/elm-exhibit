afccab95f2e99786b6162514017e62eff6f372fe
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-12 18:50:41 +1030

    Use successResponse helper

--- functions/examples.ts
+++ functions/examples.ts
@@ -1,7 +1,6 @@
 import examples from "../data/examples.json";
 import { APIGatewayEvent, Context } from 'aws-lambda';
-import { StatusCodes } from "http-status-codes";
-import {errorResponse, noIdea, successBody} from "./response";
+import {errorResponse, noIdea, successResponse} from "./response";
 import { ResponseBody } from "./types";
 
 export async function handler(event: APIGatewayEvent, context: Context): Promise<ResponseBody> {
@@ -12,7 +11,7 @@ export async function handler(event: APIGatewayEvent, context: Context): Promise
   }
 
   if (params.author && params.package) {
-    return successBody(StatusCodes.OK, { examples })
+    return successResponse({ tag: "ExamplesFetched", examples })
   } else {
     return errorResponse(noIdea)
   }
