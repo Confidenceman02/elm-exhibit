ccc83755422d35fae8d04a1c4d0d34b29b684db8
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-14 01:37:20 +1030

    Write spec for initTempSession

--- tests/redis/actions.spec.ts
+++ tests/redis/actions.spec.ts
@@ -1,21 +1,21 @@
 import redisClientResult from '../../functions/redis/client'
-import {expect, assert} from 'chai'
-import { Status } from '../../lib/result'
+import {expect} from 'chai'
+import {Status} from '../../lib/result'
 import {initTempSession} from "../../functions/redis/actions";
 import {TempSessionMeta} from "../../functions/redis/types";
 
 describe('actions', () => {
+  beforeEach(() => {
+    if(redisClientResult.Status === Status.Ok) {
+      redisClientResult.data.FLUSHALL()
+    }
+  })
   describe('initTempSession', () => {
     it('should init a temporary session', async () => {
-      if (redisClientResult.Status === Status.Ok) {
-        // const client = redisClientResult.data
-        const tempSessionMeta: TempSessionMeta = {sessionId: "1234", referer: "www.elm-exhibit.com"}
-        const tempSessionCreated = await initTempSession(tempSessionMeta)
-        expect(tempSessionCreated).to.be.true
-      } else {
-        assert.fail("Redis client not found")
-      }
+      // const client = redisClientResult.data
+      const tempSessionMeta: TempSessionMeta = {sessionId: "1234", referer: "www.elm-exhibit.com"}
+      const tempSessionInitiated = await initTempSession(tempSessionMeta)
+      expect(tempSessionInitiated).to.be.true
     })
   })
-
 })
