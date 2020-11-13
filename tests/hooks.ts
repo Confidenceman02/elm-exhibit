75bb9d946861e1f896fc22454035bf5353c404f0
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-14 01:36:57 +1030

    Add redis client env variables for tests

--- tests/hooks.ts
+++
@@ -1,9 +0,0 @@
-const redisClientResult =  require('../built-lambda/redis/client')
-const Status = require('../lib/result').Status
-
-exports.mochaHooks = {
-    afterEach: (done) => {
-        console.log("FLUSHING DB")
-        return done()
-    }
-}
--- tests/mocha.env.ts
+++ tests/mocha.env.ts
@@ -1,2 +1,4 @@
 process.env.GITHUB_CLIENT_ID ='test-client-id'
 process.env.GITHUB_CLIENT_SECRET ='test-client-secret'
+process.env.REDIS_SERVICE_PORT='6377'
+process.env.REDIS_SERVICE_IP='10.1.1.20'
