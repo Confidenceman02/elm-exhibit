596ecf0fcfa8912df252bc69e1271e3ba158adfa
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-13 17:39:31 +1030

    Add guard incase redis client doe not initiate

--- functions/redis/actions.ts
+++ functions/redis/actions.ts
@@ -1,43 +1,51 @@
-import client from "./client"
+import redisClientResult from "./client"
 import {generateExpirableDBKey, generatePermanentDBKey, resolveExpiration} from "./common";
 import {ExpirableDBTag, PermanentDBTag, TempSessionMeta} from "./types";
 import {GithubUserData, TempSession} from "../types";
 import {Table, UserSchema} from "./schema";
+import {Status} from '../../lib/result'
 
 // This will store the referer so that when the user approves the github app we can
 // redirect them back to where they tried to login from. i.e. example page
 export async function initTempSession(meta: TempSessionMeta): Promise<boolean> {
-  const dbKey = generateExpirableDBKey(ExpirableDBTag.TempSession, meta.sessionId)
-  const setSession = await client.HSETAsync(dbKey, "referer", meta.referer )
-  client.EXPIRE(dbKey, resolveExpiration(ExpirableDBTag.TempSession))
-
-  return !!setSession
+  if (redisClientResult.Status === Status.Ok) {
+    const client = redisClientResult.data
+    const dbKey = generateExpirableDBKey(ExpirableDBTag.TempSession, meta.sessionId)
+    const setSession = await client.HSETAsync(dbKey, "referer", meta.referer )
+    client.EXPIRE(dbKey, resolveExpiration(ExpirableDBTag.TempSession))
+    return !!setSession
+  }
+  return false
 }
 
 export async function initSession(sessionId: string, gitUser: GithubUserData): Promise<boolean> {
-  const dbKey = generateExpirableDBKey(ExpirableDBTag.Session, sessionId)
-  const setSession = await client.HSETAsync(dbKey, UserSchema.userName, gitUser.login, UserSchema.avatarUrl, gitUser.avatar_url)
-  client.EXPIRE(dbKey, resolveExpiration(ExpirableDBTag.Session))
-
-  return !!setSession
-}
-
-export async function getTempSession(tempSesh: TempSession) {
-  // This generated key should be an exact match to the initially generated key
-  const dbKey = generateExpirableDBKey(ExpirableDBTag.TempSession, tempSesh.sessionId)
-  const tempSessionData = await client.HGETALLAsync(dbKey)
-  return tempSessionData
+  if (redisClientResult.Status === Status.Ok) {
+    const client = redisClientResult.data
+    const dbKey = generateExpirableDBKey(ExpirableDBTag.Session, sessionId)
+    const setSession = await client.HSETAsync(dbKey, UserSchema.userName, gitUser.login, UserSchema.avatarUrl, gitUser.avatar_url)
+    client.EXPIRE(dbKey, resolveExpiration(ExpirableDBTag.Session))
+    return !!setSession
+  }
+  return false
 }
 
 export async function tempSessionExists(tempSesh: TempSession): Promise<boolean> {
-  const dbKey = generateExpirableDBKey(ExpirableDBTag.TempSession, tempSesh.sessionId)
-  const keyExists = await client.EXISTSAsync(dbKey)
-  return !!keyExists
+  if (redisClientResult.Status == Status.Ok) {
+    const client = redisClientResult.data
+    const dbKey = generateExpirableDBKey(ExpirableDBTag.TempSession, tempSesh.sessionId)
+    const keyExists = await client.EXISTSAsync(dbKey)
+    return !!keyExists
+  }
+  return false
 }
 
 export async function createUser(gitUser: GithubUserData): Promise<boolean> {
-  const userReferenceKey = generatePermanentDBKey(PermanentDBTag.User, gitUser.id.toString())
-  const setUserReference = await client.ZADDAsync(Table.users, gitUser.id, userReferenceKey)
-  const setUser = await client.HSETAsync(userReferenceKey, UserSchema.userName, gitUser.login, UserSchema.userId, gitUser.id, UserSchema.avatarUrl, gitUser.avatar_url)
-  return (!!setUserReference && !!setUser)
+  if (redisClientResult.Status === Status.Ok) {
+    const client = redisClientResult.data
+    const userReferenceKey = generatePermanentDBKey(PermanentDBTag.User, gitUser.id.toString())
+    const setUserReference = await client.ZADDAsync(Table.users, gitUser.id, userReferenceKey)
+    const setUser = await client.HSETAsync(userReferenceKey, UserSchema.userName, gitUser.login, UserSchema.userId, gitUser.id, UserSchema.avatarUrl, gitUser.avatar_url)
+    return (!!setUserReference && !!setUser)
+  }
+  return false
 }
--- functions/redis/client.ts
+++ functions/redis/client.ts
@@ -1,12 +1,20 @@
 import {Promise} from "bluebird";
 import redisLib from "redis";
 import {IPromisifiedRedis} from "./types";
+import {Result, ResultType} from "../../lib/result";
 
-const redisPort = process.env.REDIS_SERVICE_PORT ? process.env.REDIS_SERVICE_PORT : "0"
+const redisPort: string | undefined = process.env.REDIS_SERVICE_PORT ? process.env.REDIS_SERVICE_PORT : "0"
+const redisIP: string | undefined = process.env.REDIS_SERVICE_IP
 const redis = Promise.promisifyAll(redisLib)
-const client: IPromisifiedRedis = redis.createClient({
-  host: process.env.REDIS_SERVICE_IP,
-  port: parseInt(redisPort)
-})
+const client: () => ResultType<IPromisifiedRedis> = (): ResultType<IPromisifiedRedis> => {
+  if (redisIP && redisPort) {
+    const redisClient = redis.createClient({
+      host: redisIP,
+      port: parseInt(redisPort)
+    })
+    return Result<IPromisifiedRedis>().Ok(redisClient)
+  }
+  return Result().Err
+}
 
-export default client
+export default client()
