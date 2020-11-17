84060b47c44e7475339283fd5f3a9f2c54920bda
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-17 17:26:14 +1030

    Add tests for redis actions

--- tests/redis/actions.spec.ts
+++ tests/redis/actions.spec.ts
@@ -18,10 +18,32 @@ describe('actions', () => {
   }))
   describe('initTempSession', () => {
     it('should init a temporary session', async () => {
-      // const client = redisClientResult.data
-      const tempSessionMeta: TempSessionMeta = {sessionId: "1234", referer: "www.elm-exhibit.com"}
-      const tempSessionInitiated = await initTempSession(tempSessionMeta)
+      const tempSession: TempSession = {sessionId: "1234", referer: "www.elm-exhibit.com"}
+      const tempSessionInitiated = await initTempSession(tempSession)
       expect(tempSessionInitiated).to.be.true
+    })
+  })
+  describe('initSession', () => {
+    it('should init a session', async () => {
+      const sessionId = "1234"
+      const gitUserData: GithubUserData = {login: "ConfidenceMan02", id: 2345, avatar_url: 'www.bs.com'}
+      const sessionInitiated = await initSession(sessionId, gitUserData)
+      expect(sessionInitiated).to.be.true
+    })
+  })
+  describe('tempSessionExists', () => {
+    it('should find a temp session', async () => {
+      const tempSession: TempSession = {sessionId: "1234", referer: "www.elm-exhibit.com"}
+      await initTempSession(tempSession)
+      const foundTempSession = await tempSessionExists(tempSession)
+      expect(foundTempSession).to.be.true
+    })
+  })
+  describe('createUser', () => {
+    it('should create a user', async () => {
+      const gitUserData: GithubUserData = { login: "ConfidenceMan02", id: 2345, avatar_url: 'www.bs.com' }
+      const userCreated = await createUser(gitUserData)
+      expect(userCreated).to.be.true
     })
   })
 })
