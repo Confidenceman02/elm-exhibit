d8fb432c805ace5d2d30c8b4b7f521eba9547f8f
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-18 00:34:42 +1030

    Remove call to DB

--- functions/build-example.ts
+++ functions/build-example.ts
@@ -2,7 +2,6 @@ import { StatusCodes } from "http-status-codes";
 import { APIGatewayEvent, Context } from "aws-lambda";
 import {errorResponse, noIdea} from "./response";
 import { ResponseBody } from "./types";
-import redisClient from "./redis/client"
 
 export async function handler(event: APIGatewayEvent, context: Context): Promise<ResponseBody> {
   const params = event.queryStringParameters;
@@ -12,26 +11,11 @@ export async function handler(event: APIGatewayEvent, context: Context): Promise
   }
 
   if (params.author && params.package && params.example) {
-    const val = await redisClient.GETAsync("hello")
-    if (val) {
-      // temporary html resolving. Real html will come from elm compiler.
-      // const htmlString = await fs.readFile(path.resolve(process.cwd(),'data/elm.js'), "utf-8")
-      // get rid of all the new line characters etc.
-      // const minifiedHtml: string = htmlString.toString();
-
-      return {
-        statusCode: StatusCodes.OK,
-        body: "WORKED",
-        headers: {
-          "Content-Type": "text/javascript"
-        }
-      }
-    }
     return {
-      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-      body: "NOVALUESON",
+      statusCode: StatusCodes.OK,
+      body: "WORKED",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "text/javascript"
       }
     }
   }
