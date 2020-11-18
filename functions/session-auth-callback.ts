b077b7a5b1bb39d43fab60cbe7314c68c682dc00
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-18 23:58:37 +1030

    Check for user
    If the session cookie expires the user will still have an account.
    In this case we dont want to create a new user and instead just return the
    new session.
    
    If there is no user then we create one then return the session.

--- functions/session-auth-callback.ts
+++ functions/session-auth-callback.ts
@@ -3,7 +3,7 @@ import {GithubUserData, ResponseBody} from "./types";
 import {TempSession} from "./redis/types";
 import {ResultType, Status} from "../lib/result";
 import {errorResponse, noIdea, successResponse} from "./response";
-import {createUser, getSession, initSession, tempSessionExists} from "./redis/actions";
+import {createUser, getSession, initSession, tempSessionExists, userExists} from "./redis/actions";
 import {githubLoginEndpoint, githubUserEndpoint} from "./endpoint";
 import fetch from "node-fetch";
 import {acceptJson, withAuth} from "./headers";
@@ -16,30 +16,35 @@ export async function handler(event: APIGatewayEvent, context: Context): Promise
   if (!params || !params.code || !params.state || redisClient.Status === Status.Err) {
     return errorResponse(noIdea)
   }
-
   const client = redisClient.data
-
   const decodedState: string = Buffer.from(params.state, "base64").toString("utf8")
 
   try {
     const stateParamAsObject: TempSession = JSON.parse(decodedState)
     const sessionId = stateParamAsObject.sessionId
-    // make sure a temp session exists.
     const tmpSessionExists = await tempSessionExists(stateParamAsObject, client)
     const loginEndPoint: ResultType<URL> = githubLoginEndpoint(params.code)
     if (tmpSessionExists && loginEndPoint.Status === Status.Ok) {
       const response = await fetch(loginEndPoint.data.href, { method: "POST", headers: { ...acceptJson } })
       const responseData = await response.json()
-      //  get user github info
+      // get user github info
       const githubUserResponse = await fetch(githubUserEndpoint().href, { headers: { ...acceptJson, ...withAuth(responseData.access_token) } })
       const parsedGithubUserResponse: GithubUserData = await githubUserResponse.json()
+      // initiate a session
       const initiatedSession: boolean = await initSession(sessionId, parsedGithubUserResponse, client)
-      if (initiatedSession) {
-        // TODO: check for existing user based on id before creating a user
+      const session: ResultType<UserSession> = await getSession(sessionId, client)
+      if (initiatedSession && session.Status === Status.Ok) {
+        // check for a existing user as maybe the cookie expired or they revoked the app.
+        const usrExists = await userExists(parsedGithubUserResponse.id, client)
+        if (usrExists) {
+          return successResponse({ tag: "SessionGranted", session: session.data})
+        }
+        // create user if this is their first time logging in
         const createdUser: boolean = await createUser(parsedGithubUserResponse, client)
-        const session: ResultType<UserSession> = await getSession(sessionId, client)
-        // send success response
-        return successResponse({ tag: "SessionGranted", session: session.data})
+        if (createdUser) {
+          return successResponse({ tag: "SessionGranted", session: session.data})
+        }
+        return errorResponse({ tag: "LogInFailed" })
       }
       return errorResponse({ tag: "LogInFailed" })
     }
