54f8592101742de26339458253057f13caae59b6
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-11-19 00:43:40 +1030

    Use git helper to build URL

--- functions/session-grant.ts
+++ functions/session-grant.ts
@@ -1,26 +1,23 @@
-import { APIGatewayEvent, Context } from "aws-lambda";
-import { ResponseBody } from "./types";
-import { URL } from "url";
+import {APIGatewayEvent, Context} from "aws-lambda";
+import {ResponseBody} from "./types";
 import {errorResponse, successResponse} from "./response";
-import { v4 as uuidv4 } from "uuid"
+import {v4 as uuidv4} from "uuid"
 import {initTempSession} from "./redis/actions";
 import redisClient from "./redis/client"
-
-const gitHubClientId: string | undefined = process.env.GITHUB_CLIENT_ID
+import {Status} from "../lib/result";
+import {githubAuthorizeEndpoint} from "./endpoint";
 
 export async function handler(event: APIGatewayEvent, context: Context): Promise<ResponseBody> {
   const { referer } = event.headers
-  if (gitHubClientId) {
-    const sessionId = uuidv4()
-    const stateAsJSON: string = JSON.stringify({ sessionId: sessionId, referer: referer })
-    const encodedState: string = Buffer.from(stateAsJSON, "utf8").toString("base64")
-    githubAuthorizeEndpoint.searchParams.append("client_id", gitHubClientId)
-    githubAuthorizeEndpoint.searchParams.append("state", encodedState)
+  const sessionId = uuidv4()
+  const authEndpoint = githubAuthorizeEndpoint(sessionId, referer)
+  if (redisClient.Status === Status.Ok && authEndpoint.Status === Status.Ok) {
+    const client = redisClient.data
     // save temporary session meta
-    const tempSession = await initTempSession({ sessionId, referer } )
+    const tempSession = await initTempSession({ sessionId, referer }, client )
 
     if (tempSession) {
-      return successResponse({ tag: "Redirecting", location: githubAuthorizeEndpoint.href })
+      return successResponse({ tag: "Redirecting", location: authEndpoint.data.href })
     }
   }
   return errorResponse({ tag: "LogInFailed" })
