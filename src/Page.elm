58c5e8147ed6f7c6223ff006c2fc20f777edcb76
Jaime Terreu <jaimeterreu@pop-os.localdomain>
2020-12-01 01:19:35 +1030

    track header state in Page Msg

--- src/Page.elm
+++ src/Page.elm
@@ -9,7 +9,7 @@ import Session exposing (Session)
 
 type Page msg
     = Home
-    | Examples Author Package Session (Header.Msg -> msg)
+    | Examples Author Package Session (Header.Msg -> msg) Header.State
     | AuthGithubRedirect
 
 
@@ -23,10 +23,10 @@ view page { title, content } =
 viewHeader : Page msg -> Styled.Html msg
 viewHeader page =
     case page of
-        Examples author package session toHeaderMsg ->
+        Examples author package session toHeaderMsg headerState ->
             -- This looks weird but the Header has its own msgs it can dispatch so we need to
             -- catch those in the relevant Page. The Examples page in this case.
-            Styled.map toHeaderMsg (Header.view (Header.example author package |> Header.session session))
+            Styled.map toHeaderMsg (Header.view (Header.example author package |> Header.session session |> Header.state headerState))
 
         Home ->
             text ""
