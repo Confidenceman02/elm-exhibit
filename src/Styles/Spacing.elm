4b7bc12d4bcc0eb7158c7ec43dcaf273b8fccfb3
Jaime Terreu <jaime@terreu.com>
2021-03-19 20:08:05 +1030

    Add Spacing module

---
+++ src/Styles/Spacing.elm
@@ -0,0 +1,58 @@
+module Styles.Spacing exposing
+    ( exSpacingLg
+    , exSpacingMd
+    , exSpacingSm
+    , exSpacingXl
+    , exSpacingXs
+    , exSpacingXxl
+    , exSpacingXxxl
+    , exSpacingXxxxl
+    , exSpacingXxxxxl
+    )
+
+import Css exposing (Rem)
+
+
+exSpacingXs : Rem
+exSpacingXs =
+    Css.rem 0.375
+
+
+exSpacingSm : Rem
+exSpacingSm =
+    Css.rem 0.75
+
+
+exSpacingMd : Rem
+exSpacingMd =
+    Css.rem 1.5
+
+
+exSpacingLg : Rem
+exSpacingLg =
+    Css.rem 2.25
+
+
+exSpacingXl : Rem
+exSpacingXl =
+    Css.rem 3
+
+
+exSpacingXxl : Rem
+exSpacingXxl =
+    Css.rem 3.75
+
+
+exSpacingXxxl : Rem
+exSpacingXxxl =
+    Css.rem 4.5
+
+
+exSpacingXxxxl : Rem
+exSpacingXxxxl =
+    Css.rem 5.25
+
+
+exSpacingXxxxxl : Rem
+exSpacingXxxxxl =
+    Css.rem 6
