dfe20f8c21b8153d4df7e95db16544f435243c59
Jaime Terreu <jaime@terreu.com>
2021-03-21 23:46:23 +1030

    Refactor session-grant endpoint

--- functions/session-grant.ts
+++ functions/session-grant.ts
@@ -13,27 +13,28 @@ export async function handler(event: APIGatewayEvent, context: Context): Promise
   const sessionId = uuidv4()
   const resolvedReferer: string = resolveReferer(referer)
   const authEndpoint = githubAuthorizeEndpoint(sessionId, resolvedReferer)
-  if (redisClient.Status === Status.Ok) {
-    const client = redisClient.data
-    try {
-      // check for cookie
-      const cookieResult = parseCookie(cookie)
-      if (cookieResult.Status === Status.Ok) {
-        const cookie: SessionCookie = cookieResult.data
-        const sessionResult = await getSession(cookie.session_id, client)
-        if (sessionResult.Status === Status.Ok) {
-          return successResponse({ tag: "SessionGranted", session: sessionResult.data})
-        }
-      }
-      // save temporary session meta
-      const tempSession = await initTempSession({ sessionId, referer: resolvedReferer }, client )
+  const cookieResult = parseCookie(cookie)
 
-      if (tempSession && authEndpoint.Status === Status.Ok) {
-        return successResponse({ tag: "Redirecting", location: authEndpoint.data.href })
-      }
-    } catch (e) {
-      return errorResponse({ tag: "LoginFailed" })
-    }
+  if (redisClient.Status === Status.Err) {
+    return errorResponse({ tag: "LoginFailed" })
   }
-  return errorResponse({ tag: "LoginFailed" })
+
+  const client = redisClient.data
+
+  // when no cookie
+  if (cookieResult.Status === Status.Err) {
+    // save temporary session meta
+    const tempSession = await initTempSession({ sessionId, referer: resolvedReferer }, client )
+
+    if (tempSession && authEndpoint.Status === Status.Ok) {
+      return successResponse({ tag: "Redirecting", location: authEndpoint.data.href })
+    }
+    return errorResponse({ tag: "LoginFailed" })
+  }
+
+  const sessionResult = await getSession(cookieResult.data.session_id, client)
+  if (sessionResult.Status === Status.Err) {
+    return errorResponse({ tag: "LoginFailed" })
+  }
+  return successResponse({ tag: "SessionGranted", session: sessionResult.data})
 }
