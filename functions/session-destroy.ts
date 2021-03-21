0a776eba9cb08cccedf23e2d7bfbc13c3888d26b
Jaime Terreu <jaime@terreu.com>
2021-03-22 00:34:01 +1030

    Add session-destroy function

---
+++ functions/session-destroy.ts
@@ -0,0 +1,36 @@
+import {APIGatewayEvent, Context} from "aws-lambda";
+import {ResponseBody} from "./types";
+import {parseCookie} from "./request";
+import {ResultType, Status} from "../lib/result";
+import {errorResponse, noIdea, successResponse} from "./response";
+import {UserSession} from "./redis/schema";
+import {destroySession, getSession} from "./redis/actions";
+import redisClient from "./redis/client";
+
+export async function handler(event: APIGatewayEvent, context: Context): Promise<ResponseBody> {
+  const { cookie } = event.headers
+  const cookieResult = parseCookie(cookie)
+
+  if (redisClient.Status === Status.Err) {
+    return errorResponse(noIdea)
+  }
+
+  const client = redisClient.data
+
+  if (cookieResult.Status === Status.Err) {
+    return errorResponse({ tag: "MissingCookie" })
+  }
+
+  const session: ResultType<UserSession> = await getSession(cookieResult.data.session_id, client)
+
+  if (session.Status === Status.Err) {
+    return errorResponse({ tag: "SessionNotFound" })
+  }
+
+  const sessionDestroyed: boolean = await destroySession(session.data.sessionId, client)
+
+  if (sessionDestroyed) {
+    return successResponse({ tag: "SessionDestroyed" })
+  }
+  return errorResponse({ tag: "SessionNotFound" })
+}
