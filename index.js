4986ad2545021721c2062ab84e5443a50df1a149
Jaime Terreu <jaime@terreu.com>
2021-03-22 22:01:48 +1030

    Linting

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-import {Elm} from './src/Main'
+import { Elm } from "./src/Main";
 
-const app: Elm.Main.App = Elm.Main.init({node: document.querySelector('main'), flags: null});
+const app = Elm.Main.init({
+  node: document.querySelector("main"),
+  flags: null,
+});
 
-app.ports.decodeRefererFromStateParam.subscribe((base64String: string) => {
-    const decodedString = window.atob(base64String)
-    try {
-        const parsedJSON: { sessionId: string; referer: string;} = JSON.parse(decodedString)
-        app.ports.decodedRefererFromStateParam.send(parsedJSON.referer)
-    } catch (e) {
-        console.log(e)
-    }
-})
+app.ports.decodeRefererFromStateParam.subscribe((base64String) => {
+  const decodedString = window.atob(base64String);
+  try {
+    const parsedJSON = JSON.parse(decodedString);
+    app.ports.decodedRefererFromStateParam.send(parsedJSON.referer);
+  } catch (e) {
+    console.log(e);
+  }
+});
--- lib/result.ts
+++ lib/result.ts
@@ -1,18 +1,20 @@
 export enum Status {
   Err,
-  Ok
+  Ok,
 }
 
-export type ResultType<T> = ({ Status: Status.Ok, data: T } | { Status: Status.Err })
+export type ResultType<T> =
+  | { Status: Status.Ok; data: T }
+  | { Status: Status.Err };
 
 interface ResultResolver<T> {
   Err: { Status: Status.Err };
-  Ok: (arg: T) => { Status: Status.Ok, data: T }
+  Ok: (arg: T) => { Status: Status.Ok; data: T };
 }
 
 export function Result<T>(): ResultResolver<T> {
   return {
     Err: { Status: Status.Err },
-    Ok: (arg: T) => ({ Status: Status.Ok, data: arg })
-  }
+    Ok: (arg: T) => ({ Status: Status.Ok, data: arg }),
+  };
 }
